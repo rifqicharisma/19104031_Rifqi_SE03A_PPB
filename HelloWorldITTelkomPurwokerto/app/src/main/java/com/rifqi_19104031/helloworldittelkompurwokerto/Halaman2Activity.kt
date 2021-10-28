c52b3ea4c937df6f20d366e03a9aedaca7e0c9ab
Rifqi Charisma <rifqibarker@gmail.com>
2021-10-28 17:38:24 +0700

    update modul3

---
+++ HelloWorldITTelkomPurwokerto/app/src/main/java/com/rifqi_19104031/helloworldittelkompurwokerto/Halaman2Activity.kt
@@ -0,0 +1,11 @@
+package com.rifqi_19104031.helloworldittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Halaman2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman2)
+    }
+}
