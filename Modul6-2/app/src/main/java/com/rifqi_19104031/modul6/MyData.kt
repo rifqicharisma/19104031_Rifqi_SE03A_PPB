b075c0b6932a69fa61000c6ce4d13ac02c49e1a9
Rifqi Charisma <rifqibarker@gmail.com>
2022-01-04 12:40:19 +0700

    update modul 6

--- Modul6-2/app/src/main/java/com/rifqi_19104031/modul6/MyData.kt
+++ Modul6-2/app/src/main/java/com/rifqi_19104031/modul6/MyData.kt
@@ -1,7 +1,10 @@
 package com.rifqi_19104031.modul6
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class MyData(
     var name : String,
     var description : String,
     var photo : String
-)
+) : Parcelable
