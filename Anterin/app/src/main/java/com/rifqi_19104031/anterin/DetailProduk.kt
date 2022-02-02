de487a9294c6e1d37095856e98b682de8cbcd41f
Ananda Aulia <72482619+Ananda28@users.noreply.github.com>
2022-02-02 19:30:12 +0700

    create detailproduk

---
+++ Anterin/app/src/main/java/com/rifqi_19104031/anterin/DetailProduk.kt
@@ -0,0 +1,11 @@
+package com.rifqi_19104031.anterin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailProduk : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
