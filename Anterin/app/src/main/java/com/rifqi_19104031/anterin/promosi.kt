ce4804ba3c349e5b6dd41471691bc6600ef5dfff
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2022-02-03 08:28:42 +0700

    Create Promotion Page

---
+++ Anterin/app/src/main/java/com/rifqi_19104031/anterin/promosi.kt
@@ -0,0 +1,11 @@
+package com.rifqi_19104031.anterin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class promosi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promosi)
+    }
+}
