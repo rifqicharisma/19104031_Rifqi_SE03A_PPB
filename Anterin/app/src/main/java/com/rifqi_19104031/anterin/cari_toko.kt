763d33d434cebad83c84297fbc8508cad67db0e9
FAUZIYAH ULUR ROSYAD PRAWIDANI <fprawidani@gmail.com>
2022-02-03 09:58:46 +0700

    cari toko

---
+++ Anterin/app/src/main/java/com/rifqi_19104031/anterin/cari_toko.kt
@@ -0,0 +1,11 @@
+package com.rifqi_19104031.anterin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cari_toko : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cari_toko)
+    }
+}
