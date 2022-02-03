1d310fd65875e8e5bea1bec8c1c4c36138edfa09
Rifqi Charisma <rifqibarker@gmail.com>
2022-02-03 11:03:44 +0700

    update profile

---
+++ Anterin/app/src/main/java/com/rifqi_19104031/anterin/profil.kt
@@ -0,0 +1,11 @@
+package com.rifqi_19104031.anterin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
